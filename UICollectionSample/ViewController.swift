897412ae8fddcee58104f1029691a4504bdfb9fc
hira22 <masayuki.hiraoka@ietty.co.jp>
2020-06-29 13:56:31 +0900

    Initial Commit

---
+++ UICollectionSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UICollectionSample
+//
+//  Created by hiraoka on 2020/06/29.
+//  Copyright © 2020 hiraoka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
